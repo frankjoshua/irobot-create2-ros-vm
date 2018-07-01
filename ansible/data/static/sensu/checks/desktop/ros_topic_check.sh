373d6f596574fb7d5f0937a46256cea1d7a4b8ac
josh <josh@tesseractmobile.com>
2018-07-01 01:20:41 -0500

    Fixe ROS log directory

--- ansible/data/static/sensu/checks/desktop/ros_topic_check.sh
+++ ansible/data/static/sensu/checks/desktop/ros_topic_check.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+#Setup ROS enviroment for the check to run
 source /opt/ros/indigo/setup.bash
+#Change log directory to a writable one
+export ROS_LOG_DIR=/var/log/sensu
+#Set a small timeout and echo the ROS topic
 timeout 2 rostopic echo -n 1 $1 || exit 2
